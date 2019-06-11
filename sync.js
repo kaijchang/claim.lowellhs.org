4337b2f93e8d32f3002c849e93f743183ca1eab7
Kai Chang <kaijchang@gmail.com>
2019-06-10 22:21:48 -0700

    fix: include node_modules

--- sync.js
+++ sync.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-
 const hosts = require('./hosts');
 
-fs.writeFileSync('hosts.json', hosts.sort((a, b) => a.localeCompare(b)));
+fs.writeFileSync('hosts.json', JSON.stringify(hosts.sort((a, b) => a.Address.localeCompare(b.Address)), null, 2));
