a9e00363d3607efc940514e5993999d35e5d6bb8
Kai Chang <kaijchang@gmail.com>
2019-06-10 22:46:40 -0700

    fix: order by HostName

--- sync.js
+++ sync.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const hosts = require('./hosts');
 
-fs.writeFileSync('hosts.json', JSON.stringify(hosts.sort((a, b) => a.Address.localeCompare(b.Address)), null, 2));
+fs.writeFileSync('hosts.json', JSON.stringify(hosts.sort((a, b) => a.HostName.localeCompare(b.HostName)), null, 2));
