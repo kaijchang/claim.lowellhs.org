f7c72e6e71a729411649869a1ba6e10d6753080f
Kai Chang <kaijchang@gmail.com>
2019-06-11 11:49:18 -0700

    feat: projects page

---
+++ docs/scripts/projects.js
@@ -0,0 +1,9 @@
+fetch('https://raw.githubusercontent.com/kajchang/lowellhs.org/master/hosts.json')
+    .then(res => res.json())
+    .then(hosts => {
+        hosts.filter(host => host.HostName !== '@' && host.HostName !== 'www').forEach(project => {
+            const li = document.createElement('li');
+            li.innerHTML = `<a href="http://${ project.HostName }.lowellhs.org">${ project.HostName }.lowellhs.org</a>`;
+            document.getElementById('projects').appendChild(li);
+        });
+    });
