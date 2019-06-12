2e8f95961976151997e9c2c8f4fdf40f9c940cb3
Kai Chang <kaijchang@gmail.com>
2019-06-11 21:15:50 -0700

    feat: update docs

--- docs/scripts/subdomains.js
+++ docs/scripts/subdomains.js
@@ -4,6 +4,6 @@ fetch('https://raw.githubusercontent.com/kajchang/lowellhs.org/master/hosts.json
         hosts.filter(host => host.HostName !== '@' && host.HostName !== 'www').forEach(project => {
             const li = document.createElement('li');
             li.innerHTML = `<a href="http://${ project.HostName }.lowellhs.org" rel="noopener" target="_blank">${ project.HostName }.lowellhs.org</a>`;
-            document.getElementById('projects').appendChild(li);
+            document.getElementById('subdomains').appendChild(li);
         });
     });
