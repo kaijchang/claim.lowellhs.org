f638a97724c35dba06353491dece58c37c8ccd9b
Kai Chang <kaijchang@gmail.com>
2019-12-07 20:48:24 -0800

    fix documentation website

--- docs/scripts/subdomains.js
+++ docs/scripts/subdomains.js
@@ -1,7 +1,7 @@
 fetch('https://raw.githubusercontent.com/kajchang/lowellhs.org/master/hosts.json')
     .then(res => res.json())
     .then(hosts => {
-        hosts.filter(host => host.HostName !== '@' && host.HostName !== 'www').forEach(project => {
+        hosts.filter(host => host.HostName !== '@' && host.HostName !== 'claim').forEach(project => {
             const li = document.createElement('li');
             li.innerHTML = `<a href="http://${ project.HostName }.lowellhs.org" rel="noopener" target="_blank">${ project.HostName }.lowellhs.org</a>`;
             document.getElementById('subdomains').appendChild(li);
